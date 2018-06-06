680331667032fd88acc8abbfbd15fcbb312bd26c
helioboros <felinewraith@gmail.com>
2018-06-06 14:37:51 -0400

    began controllers

---
+++ routes/eventController.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const router = express.Router({mergeParams: true})
+// const, fuckin, idk, people = require ../models/People
+
+//GET event listing
+router.get("/", (req, res) => {
+
+})
+
+//NEW route
+
+//CREATE route
+
+//SHOW route
+
+//EDIT route
+
+//UPDATE route
+
+//DELETE route
+
+module.exports = router;
