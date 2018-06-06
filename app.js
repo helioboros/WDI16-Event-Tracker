b62f4b1470cfce7f57e5a91d1fc3f248870caf15
helioboros <felinewraith@gmail.com>
2018-06-06 11:24:27 -0400

    Updated app.js with necessary middleware and route connections

--- app.js
+++ app.js
@@ -3,16 +3,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const methodOverride = require("method-override")
+const mongoose = require("mongoose")
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Commented out code has their files under WIP.
+// var userController = require('./routes/userController');
 
 var app = express();
+
+//connect to db
+mongoose.connect("mongodb://localhost/event-tracker")
+  .then (() => {
+    console.log("connected to mongoDB")
+  })
+  .catch((err) => {
+    console.log("ERROR", err)
+  })
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use(methodOverride("_method"))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,7 +33,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', userController);
+
+// app.use("/people", peopleController)
+// app.use("/people/:peopleId/dates", dateController)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
