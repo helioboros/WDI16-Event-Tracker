4f854a8a238a726f106d5d1d8efeb26d2c53a5c3
helioboros <felinewraith@gmail.com>
2018-06-07 10:54:16 -0400

    added people/event controllers

--- routes/eventController.js
+++ routes/eventController.js
@@ -1,22 +1,59 @@
 const express = require("express")
 const router = express.Router({mergeParams: true})
-// const, fuckin, idk, people = require ../models/People
+const Event = require("../models/Event")
 
 //GET event listing
-router.get("/", (req, res) => {
-
-})
+router.get('/', function (req, res, next) {
+    Event.find()
+      .then((events) => {
+        res.render("events/index", {
+          events
+        })
+      })
+  });
 
 //NEW route
-
+router.get("/new", (req, res) => {
+    res.render("/events/new")
+})
 //CREATE route
-
+router.get("/", (req, res) => {
+    const newEvent = req.body
+    Event.create(newEvent)
+      .then(() => {
+        res.redirect("/events")
+      })
+  })
 //SHOW route
-
+router.get("/:id", (req, res) => {
+    Event.findById(req.params.id)
+      .then((singleEvent) => {
+        res.render("events/show", {
+          singleEvent
+        })
+      })
+  })
 //EDIT route
-
+router.get("/:id/edit", (req, res) => {
+    Event.findById(req.params.id)
+      .then((singleEvent) => {
+        res.render("events/edit", { singleEvent })
+      })
+  })
 //UPDATE route
-
+router.put("/:id", (req, res) => {
+    Event.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then(() => {
+      res.redirect(`/events/${req.params.id}`)
+    })
+  })
 //DELETE route
+router.delete("/:id", (req, res) => {
+    Event.findByIdAndRemove(req.params.id)
+    .then(() => {
+    res.redirect("/events") 
+    })
+  })
+
 
 module.exports = router;
--- routes/peopleController.js
+++ routes/peopleController.js
@@ -1,22 +1,58 @@
 const express = require("express")
 const router = express.Router({mergeParams: true})
-// const, fuckin, idk, people = require ../models/People
+const People = require("../models/People")
 
-//GET event listing
-router.get("/", (req, res) => {
-
-})
+//GET person listing
+router.get('/', function (req, res, next) {
+    People.find()
+      .then((people) => {
+        res.render("people/index", {
+          people
+        })
+      })
+  });
 
 //NEW route
-
+router.get("/new", (req, res) => {
+    res.render("/people/new")
+})
 //CREATE route
-
+router.get("/", (req, res) => {
+    const newPerson = req.body
+    People.create(newPerson)
+      .then(() => {
+        res.redirect("/people")
+      })
+  })
 //SHOW route
-
+router.get("/:id", (req, res) => {
+    People.findById(req.params.id)
+      .then((person) => {
+        res.render("people/show", {
+          person
+        })
+      })
+  })
 //EDIT route
-
+router.get("/:id/edit", (req, res) => {
+    People.findById(req.params.id)
+      .then((person) => {
+        res.render("people/edit", { person })
+      })
+  })
 //UPDATE route
-
+router.put("/:id", (req, res) => {
+    People.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then(() => {
+      res.redirect(`/people/${req.params.id}`)
+    })
+  })
 //DELETE route
+router.delete("/:id", (req, res) => {
+    People.findByIdAndRemove(req.params.id)
+    .then(() => {
+    res.redirect("/people") 
+    })
+  })
 
 module.exports = router;
