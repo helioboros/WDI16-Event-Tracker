2b0bacf61088abba2b84a27797ec334ad38e313d
helioboros <felinewraith@gmail.com>
2018-06-07 13:00:25 -0400

    added user controllers

--- routes/userController.js
+++ routes/userController.js
@@ -1,22 +1,63 @@
-const express = require("express")
-const router = express.Router({mergeParams: true})
+const express = require('express');
+const router = express.Router();
 const User = require("../models/User")
 
-//GET user listing
-router.get("/", (req, res) => {
-
-})
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    User.find()
+        .then((users) => {
+            res.render("users/index", {
+                users
+            })
+        })
+});
 
 //NEW route
+router.get("/new", (req, res) => {
+    res.render("users/new")
+})
 
 //CREATE route
+router.get("/", (req, res) => {
+    const user = req.body
+    User.create(user)
+        .then(() => {
+            res.redirect("/users")
+        })
+})
 
 //SHOW route
+router.get("/:id", (req, res) => {
+    User.findById(req.params.id)
+        .then((user) => {
+            res.render("users/show", {
+                user
+            })
+        })
+})
 
 //EDIT route
+router.get("/:id/edit", (req, res) => {
+    User.findById(req.params.id)
+        .then((user) => {
+            res.render("users/edit", { user })
+        })
+})
 
 //UPDATE route
+router.put("/:id", (req, res) => {
+    User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then(() => {
+            res.redirect(`/users/${req.params.id}`)
+        })
+})
 
 //DELETE route
+router.delete("/:id", (req, res) => {
+    User.findByIdAndRemove(req.params.id)
+        .then(() => {
+            res.redirect("/users")
+        })
+})
 
 module.exports = router;
